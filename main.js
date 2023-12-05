e053a7afc11ff2c8be8acb804fe1775f3937781b
BrunoNunes08 <133694839+BrunoNunes08@users.noreply.github.com>
2023-12-05 10:31:00 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 const audioParabens = new Audio("assets/xuxa-parabens.mp3");
 const audioBoraTomarUma = new Audio("assets/bora-tomar-uma.mp3");
 audioParabens.loop = true;
-alert("Clica na tela após fechar essa mensagem, menos em cima do botão (só clica no botão depois de ler o texto)!")
-window.onclick = (e) => {
-    audioParabens.play();
-}
 
 const button = document.querySelector(".sparkle-button");
 button.addEventListener("click", (e) => {
